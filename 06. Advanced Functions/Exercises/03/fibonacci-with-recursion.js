f6b7beaacb4473fa8840c7dab35b50080b3316a2
Shenol07 <shenolshengov41@gmail.com>
2025-01-24 16:31:01 +0200

    Add solutoin for task 03 (fibonacci) with recursion

---
+++ 06. Advanced Functions/Exercises/03/fibonacci-with-recursion.js
@@ -0,0 +1,42 @@
+function getFibonator() {
+    const memoization = {};
+    let number = 0;
+
+    function fibonacci(number) {
+        if(number <= 1) {
+            return number;
+        }
+        if(!memoization[number]) {
+            memoization[number] = fibonacci(number - 1) + fibonacci(number - 2);
+        }
+        return memoization[number];
+    }
+
+    return () => fibonacci(number++);
+}
+
+
+
+
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
